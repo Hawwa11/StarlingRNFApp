f58dcfec09422034f5e54dcf655c51c6ae966403
Hawwa11 <46769516+Hawwa11@users.noreply.github.com>
2020-10-27 13:03:59 +0800

    Edit and Delete Reservation - Hawwa

--- app/src/main/java/com/example/android/starlingrnfapp/EditReservation.java
+++ app/src/main/java/com/example/android/starlingrnfapp/EditReservation.java
@@ -10,5 +10,7 @@ public class EditReservation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_reservation);
+
+        
     }
 }
