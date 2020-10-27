208b7dac331a7f94508034d3e27de016a9c5ddc7
Hawwa11 <46769516+Hawwa11@users.noreply.github.com>
2020-10-27 16:04:19 +0800

    Hawwa Made changes in reservation form and confirm details. Corrected errors in edit reservation. Edited validations in Signup page. Minor layour changes. Editted Feedbak page.

--- app/src/main/java/com/example/android/starlingrnfapp/Feedback.java
+++ app/src/main/java/com/example/android/starlingrnfapp/Feedback.java
@@ -3,12 +3,24 @@ package com.example.android.starlingrnfapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class Feedback extends AppCompatActivity {
-
+RatingBar service, food, hygiene;
+Button submit;
+EditText review;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        service = findViewById(R.id.rtservice);
+        food = findViewById(R.id.rtfood);
+        hygiene = findViewById(R.id.rthygiene);
+        submit = findViewById(R.id.submitfeedback);
+        review = findViewById(R.id.reviewbox);
+
     }
 }
